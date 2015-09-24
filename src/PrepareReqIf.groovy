dfaa7bfc6bd6a33ee629037eb9c39afc150c9ec3
yaroslav <yaroslavTir@gmail.com>
2015-09-24 12:32:15 +0200

    first working version

--- src/PrepareReqIf.groovy
+++ src/PrepareReqIf.groovy
@@ -1,18 +1,25 @@
+import groovy.xml.MarkupBuilder
+
 /**
- * Created by YMolodkov on 23.09.2015.
+ * @author yaroslavTir
  */
 class PrepareReqIf {
 
     def extIdToFolderId = [:]
+    private String inFile
+    private String outFile
+
+    PrepareReqIf(String inFile, String outFile) {
+        this.inFile = inFile
+        this.outFile = outFile
+    }
 
     public static void main(String[] args) {
-        String inFile = args[0];
-        String outFile = "out.xml";
-        def prepareReqIf = new PrepareReqIf()
+        def prepareReqIf = new PrepareReqIf(args[0], "out.xml")
 
-        Node xml = prepareReqIf.open(inFile)
+        Node xml = prepareReqIf.open()
         prepareReqIf.execute(xml)
-        prepareReqIf.save(xml, outFile)
+        prepareReqIf.save(xml)
     }
 
 
@@ -40,7 +47,9 @@ class PrepareReqIf {
         def extIdToFolder = fillFolderData(xml)
         def extIdToSpec = fillSpecificationData(xml)
         def extFolderIdToExtSpecId = fillFolderSpecAssciationData(xml)
-        replace(extIdToFolder, extIdToSpec, extFolderIdToExtSpecId)
+        def specFolderPairs = createSpecFolderPairs(extFolderIdToExtSpecId, extIdToFolder, extIdToSpec)
+        replace(specFolderPairs)
+//        replace2(xml, specFolderPairs)
     }
 
     private def fillFolderData(xml) {
@@ -83,17 +92,21 @@ class PrepareReqIf {
         map
     }
 
-    private void replace(extIdToFolder, extIdToSpec, extFolderIdToExtSpecId) {
-        def specToFolder = [:]
+    private def createSpecFolderPairs(extFolderIdToExtSpecId, extIdToFolder, extIdToSpec) {
+        def specFolderPairs = [:]
         extFolderIdToExtSpecId.each { folderId, specId ->
             def folder = extIdToFolder[folderId]
             def spec = extIdToSpec[specId]
             if (folder && spec) {
-                specToFolder.put(spec, folder)
+                specFolderPairs.put(spec, folder)
             }
         }
-        println "${specToFolder.size()} replaces"
-        specToFolder.each { spec, folder ->
+        specFolderPairs
+    }
+
+    private void replace(specFolderPairs) {
+        println "${specFolderPairs.size()} replaces"
+        specFolderPairs.each { spec, folder ->
             def longName = spec."@LONG-NAME"
             def desc = folder.@DESC
             spec."@LONG-NAME" = "${longName} (${desc})"
@@ -102,12 +115,56 @@ class PrepareReqIf {
     }
 
 
-    private Node open(String inFile) {
+    private void replace2(xml, specFolderPairs) {
+        def specs = xml."CORE-CONTENT"."REQ-IF-CONTENT"."SPECIFICATIONS"[0]
+        def xmlOutput = new StringWriter()
+        specs.name().namespaceURI = ""
+        new XmlNodePrinter(new PrintWriter(xmlOutput)).print(specs)
+        def specXML = xmlOutput.toString()
+
+        def replaceMap = [:]
+        specFolderPairs.each { spec, folder ->
+            def oldLongName = spec."@LONG-NAME"
+            def newLongName = "${oldLongName} (${folder.@DESC})"
+            replaceMap.put("LONG-NAME=\"${oldLongName}\"", "LONG-NAME=\"${newLongName}\"")
+        }
+        boolean isSpecCection = false;
+        boolean isSpecPrinted = false
+        new File(outFile).withWriter { w ->
+            new File(inFile).eachLine { line ->
+                if (!isSpecCection) isSpecCection = line.contains("<SPECIFICATIONS>")
+                if (isSpecCection && line.contains("</SPECIFICATIONS>")) {
+                    isSpecCection = false
+                } else {
+                    if (isSpecCection && !isSpecPrinted) {
+                        w.println xmlOutput.toString()
+                        isSpecPrinted = true
+                    }
+                    if (!isSpecCection) {
+                        w.println line
+                    }
+                }
+            }
+        }
+    }
+
+    private String replaceLine(def replaceMap, String line) {
+        String newLine = line
+        replaceMap.each { oldValue, newValue ->
+            newLine = newLine.replaceAll(oldValue, newValue);
+        }
+        newLine
+    }
+
+
+    private Node open() {
         def xml = new XmlParser().parse(inFile)
         xml
     }
 
-    private save(Node xml, outFile) {
-        new XmlNodePrinter(new PrintWriter(new FileWriter(outFile))).print(xml)
+    private save(Node xml) {
+        def xmlOutput = new StringWriter()
+        new XmlNodePrinter(new PrintWriter(xmlOutput)).print(xml)
+        new File(outFile).write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + xmlOutput.toString())
     }
 }
